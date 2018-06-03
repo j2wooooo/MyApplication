9861012f0ae55a00af7bdba0fe223bc07968ef99
parkjiwoo <j2wooooo@naver.com>
2018-06-03 19:06:59 +0900

    add signup image function

--- app/src/main/java/com/example/myapplication/SignupActivity.java
+++ app/src/main/java/com/example/myapplication/SignupActivity.java
@@ -1,13 +1,17 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.myapplication.model.UserModel;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -16,14 +20,21 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.UploadTask;
 
 public class SignupActivity extends AppCompatActivity {
 
+
+    private static final int PICK_FROM_ALBUM = 10;
     private EditText email;
     private EditText name;
     private EditText password;
     private Button signup;
     private String splash_background;
+    // 프로필 작업
+    private ImageView profile;
+    private Uri imageUri;
 
     // firebase를 이용할 떄 항상 중요한 것은 bradle에 dependency되는 api들의 version이다.
     // 서로 version이 맞는 지 확인하자!! error가 난다면 이곳에서 났을 확률이 99%이다.
@@ -51,6 +62,18 @@ public class SignupActivity extends AppCompatActivity {
         // tool -> firebase -> authentication -> 2) add firebase authentication to your app을 해준다.
         // 권한을 얻는 데에 필요한 firebase api가 추가된다.
 
+
+        profile = (ImageView)findViewById(R.id.signupActivity_imageview_profile);
+        profile.setOnClickListener(new View.OnClickListener(){
+            @Override
+            // 프로필 클릭 시 앨범에 접근
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(intent, PICK_FROM_ALBUM);
+            }
+        });
+
         // 회원가입에 필요한 email, name, password와 signup 버튼을 불러온다.
         email = (EditText) findViewById(R.id.signupActivity_edittext_email);
         name = (EditText) findViewById(R.id.signupActivity_edittext_name);
@@ -61,7 +84,7 @@ public class SignupActivity extends AppCompatActivity {
         // signup button을 누르면 작성된 이메일, 비밀번호로 회원가입이 된다.
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
 
                 // text가 비워져 있는 란이 있다면 return한다. 회원가입 xx
                 // password가 6자리 미만일 때에는 에러가 난다.
@@ -70,22 +93,46 @@ public class SignupActivity extends AppCompatActivity {
                     return; }
 
                 FirebaseAuth.getInstance()
-                        .createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
-                        .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
+                            .createUserWithEmailAndPassword(email.getText().toString(),password.getText().toString())
+                            .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
 
-                            // 회원가입이 완료되면 => firebase에 권한이 올라가고 나서 complete으로 넘어온다.
-                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                // 회원가입이 완료되면 => firebase에 권한이 올라가고 나서 complete으로 넘어온다
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
 
-                                // 새로운 유저모델을 만들어 사용자의 uid으로 ui를 저장한다.
-                                UserModel userModel = new UserModel();
-                                userModel.userName = name.getText().toString();
+                                 // 사용자마다 고유의 uid값이 나오기 때문에 중복될 일이 없다.
+                                final String uid = task.getResult().getUser().getUid();
 
-                                String uid = task.getResult().getUser().getUid();
-                                FirebaseDatabase.getInstance().getReference().child("users").child(uid).setValue(userModel);
+                                FirebaseStorage.getInstance().getReference().child("userImages").child(uid).putFile(imageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+
+                                   @Override
+                                   public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                                        // 파일이 저장되면 저장된 경로를 task로 보내준다
+                                        String imageUrl = task.getResult().getDownloadUrl().toString();
+
+                                        // 새로운 유저모델을 만들어 사용자의 uid으로 ui를 저장한다.
+                                        UserModel userModel = new UserModel();
+                                        userModel.userName = name.getText().toString();
+                                        userModel.profieImageUrl = imageUrl;
+
+                                        FirebaseDatabase.getInstance().getReference().child("users").child(uid).setValue(userModel);
+
+                                    }
+                                });
+
                             }
                         });
             }
         });
+    }
+
+    // 앨범의 사진 선택하여 프로필 사진으로 가져온다, 이미지의 경로를 저장한다.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(requestCode == PICK_FROM_ALBUM && resultCode ==RESULT_OK){
+            profile.setImageURI(data.getData()); // 가운데 뷰를 바꿈
+            imageUri = data.getData();// 이미지 경로 원본
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/model/UserModel.java
+++ app/src/main/java/com/example/myapplication/model/UserModel.java
@@ -2,4 +2,5 @@ package com.example.myapplication.model;
 
 public class UserModel {
     public String userName;
+    public String profieImageUrl;
 }
